8a8db95aa377ca35999d5e4e3a7768743d7de8ee
adalix <gumustolgahan@gmail.com>
2024-04-12 02:16:57 +0300

    tags component added to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Tags from "./Tags";
 const Navbar = () => {
   return (
     <nav className=" mt-8 mb-5 mx-auto flex flex-col">
@@ -16,9 +17,7 @@ const Navbar = () => {
         <div className="username bg-gray-200 w-9 h-9 rounded-full text-teal-600 text-lg flex items-center justify-center ring-4 ring-teal-500 cursor-pointer">ig</div>
       </div>
 
-      <div className="tags w-full pt-5 flex items-center">
-        <a href="" className="tagBtn decoration-none py-3 px-4 rounded-3xl bg-teal-500 text-white border border-solid border-transparent transition-all ease-in-out mt-3 mr-8 cursor-pointer hover:text-teal-900 hover:bg-teal-300">All</a>
-      </div>
+      <Tags/>
     </nav>
   );
 };
