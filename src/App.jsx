cbe47cb58771fae71081a4f85e58d08a4b0884ec
adalix <gumustolgahan@gmail.com>
2024-04-11 05:05:56 +0300

    added img tag and style to it

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ const App = () => {
     <>
       <div className="w-[1300px] mx-auto my-0">
         <Navbar/>
-        <div className="imgContainer"></div>
+        <div className="imgContainer column-5 gap-4">
+          <div className="box">
+            <img src="" className="h-auto w-full rounded cursor-pointer" alt="" />
+          </div>
+        </div>
       </div>
 
       <div className="overlay"></div>
