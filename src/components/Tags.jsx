60911bca75963d9f99b71c1e7efaa7cd028fb271
adalix <gumustolgahan@gmail.com>
2024-04-12 02:12:42 +0300

    added tags component

---
+++ src/components/Tags.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tags = () => {
+  return (
+    <div className="tags w-full pt-5 flex items-center">
+      <a
+        href=""
+        className="tagBtn decoration-none py-3 px-4 rounded-3xl bg-teal-500 text-white border border-solid border-transparent transition-all ease-in-out mt-3 mr-8 cursor-pointer hover:text-teal-900 hover:bg-teal-300"
+      >
+        All
+      </a>
+    </div>
+  );
+};
+
+export default Tags;
