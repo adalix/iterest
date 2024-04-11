b609ec3c1c5af7900004ef4c59ab479e8d73a132
adalix <gumustolgahan@gmail.com>
2024-04-12 02:18:20 +0300

    fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const url = "/src/data.json";
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const datas = await response.json();
+      setData(datas);
+    };
+    fetchData();
+  },[]);
+
+
   return (
     <>
       <div className="w-[1300px] mx-auto my-0">
-        <Navbar/>
-        <div className="imgContainer column-5 gap-4">
-          <div className="box">
-            <img src="" className="h-auto w-full rounded cursor-pointer" alt="" />
-          </div>
+        <Navbar />
+        <div className="columns-5 gap-4">
+          {data.map((d) =>(
+            <Card key={d.id} src={d.url} name={d.name} />
+          ))}
         </div>
       </div>
 
