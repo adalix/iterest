4739615f58a0d738361ff3a99aedd810fd73eadc
adalix <gumustolgahan@gmail.com>
2025-01-23 15:20:43 +0300

    added: loader and its style

--- script/getdata.js
+++ script/getdata.js
@@ -1,5 +1,15 @@
 const getImages = async () => {
+  showLoader()
   const response = await fetch("/data/data.json");
   const data = await response.json();
+  hideLoader()
   return data;
 };
+
+function showLoader(){
+  document.querySelector('.loader').style.display = "block"
+}
+
+function hideLoader(){
+  document.querySelector('.loader').style.display = "none"
+}
